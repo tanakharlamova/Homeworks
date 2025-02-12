cae0762c6fba58aa69fc44d42b6beb4f92bffc42
“tanakharlamova” <tanja.kharlamova@gmail.com>
2025-02-12 17:37:19 +0200

    added 21 week react

---
+++ week21_react/chart-app/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import { Chart } from "react-google-charts";
+
+function App() {
+  
+  const data = [
+    ["Task", "Hours per Day"],
+    ["Work", 9],
+    ["Eat", 2],
+    ["Commute", 2],
+    ["Watch TV", 2],
+    ["Sleep", 7],
+  ];
+
+  const options = {
+    title: "My Daily Activities",
+  };
+  return (
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
+  );
+}
+
+export default App;
